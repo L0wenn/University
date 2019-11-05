7b49748af594993ad61d5fcaf799d85a75cada31
L0wenn <37480555+L0wenn@users.noreply.github.com>
2019-11-05 09:59:12 +0300

    Add files via upload

---
+++ 100Matches.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+int main()
+{
+	int playerAmount, totalMatches = 0;
+	bool lastTurn = true;
+
+	while(totalMatches < 100)
+	{	
+		cout << "\nTotal Score: " << totalMatches << endl;
+		cout << "\nYour turn(from 1 to 10): ";
+		while (true)
+		{
+			cin >> playerAmount;
+			if (playerAmount > 10 || playerAmount <= 0)
+				cout << "Only from 1 to 10: ";
+			else
+				break;
+		}
+		totalMatches += playerAmount;
+		lastTurn = true;
+
+		if (totalMatches >= 100)
+			break;
+
+		playerAmount = rand() % 10 + 1;
+		totalMatches += playerAmount;
+		cout << "Bot turn: " << playerAmount << endl;
+		lastTurn = false;
+	}
+
+	if (lastTurn)
+		cout << "\n\nPlayer wins!";
+	else
+		cout << "\n\nBot wins!";
+
+	return 0;
+}
+
